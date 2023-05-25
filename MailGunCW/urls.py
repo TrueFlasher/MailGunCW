d178c68f292ee33c3a16fd784a21b9310be80dae
TrueFlasher <91925260+TrueFlasher@users.noreply.github.com>
2023-05-25 04:16:00 +0300

    Update urls.py

--- MailGunCW/urls.py
+++ MailGunCW/urls.py
@@ -1,12 +1,11 @@
-from django.urls import include, path
+from django.urls import path
 from mail.views import send_email_view, registration, login_view, welcome, view_messages, dashboard, view_sent_messages
 
 urlpatterns = [
-    path('', include('MailGunCW.urls')),
     path('send_email/', send_email_view, name='send_email'),
     path('registration/', registration, name='registration'),
     path('login/', login_view, name='login'),
-    path('welcome/', welcome, name='welcome'),
+    path('', welcome, name='welcome'),
     path('view_messages/', view_messages, name='view_messages'),
     path('dashboard/', dashboard, name='dashboard'),
     path('view_sent_messages/', view_sent_messages, name='view_sent_messages'),
